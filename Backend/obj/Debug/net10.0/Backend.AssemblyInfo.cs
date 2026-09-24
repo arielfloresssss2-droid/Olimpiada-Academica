30e08af97f4b23441e088a7b1a77060c198e581e
luchocao09 <luchocao09@gmail.com>
2026-09-24 12:47:08 -0300

    resolver conflictos
    resolucion de conflictos con la base de datos y endpoints

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db93a1ab2f599cffe420134075260f1d913c7557")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a877374256212689b181ceac4fa3ca062bbcb37d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
